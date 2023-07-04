e8af0f3f994ae92ce56bcaffd4374adfb57a7060
Sneh85 <monster11heaven@gmail.com>
2023-07-04 16:58:25 +0530

    movie completed

---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,61 @@
+import "./style.scss";
+import useFetch from "../../../hooks/useFetch";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Img from "../../../components/lazyLoadImage/img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+
+const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+
+  const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
+  const { data, loading } = useFetch("/movie/upcoming");
+
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
+
+  const searchQueryHnadler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+
+  return (
+    <div className="heroBanner">
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      )}
+
+      <div className="opacity-layer"></div>
+
+      <ContentWrapper>
+        <div className="heroBannerContent">
+          <span className="title">Welcome</span>
+          <span className="subTitle">
+            want to discover movies, webshows, you are at heaven
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or tv show...."
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHnadler}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default HeroBanner;
